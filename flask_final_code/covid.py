6871d719cebfba2a63a8c7b55bb215d9a007bb26
Manos Zeakis <ezeakis@gmail.com>
2021-08-28 04:15:06 +0300

    Update covid.py

--- flask_final_code/covid.py
+++ flask_final_code/covid.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from flask import render_template
+#?
+from app import app
 
+#?
+app = Flask(__name__)
+
+@app.route("/")
+def data_input():
+#    return "<p>Hello, World!</p>"
+#    return render_template('index.html', title='Home', user=user)  
+    return render_template('index.html', title='Home')  

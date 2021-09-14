6ea9f0ade19c951175f08ee2fcb631b338e11038
ezeakis <ezeakis@gmail.com>
2021-09-14 06:40:48 +0300

    ασδ

---
+++ flask_final_code_specific_features/covid.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+#?
+#from app import app
+
+#?
+app = Flask(__name__)
+
+#app.static_folder = 'tmp/static'
+
+
+@app.route("/")
+def data_input():
+#    return "<p>Hello, World!</p>"
+#    return render_template('index.html', title='Home', user=user)  
+    return render_template('./index.html', title='Home')  
+
+@app.route("/output", methods=['POST',])
+def data_output():
+    return render_template('output.html', title='Output')  
+
+
+app.run(host='0.0.0.0', port=81)

1358f2c2642fc72d3414a608d37c5bae3a5c700f
Manos Zeakis <ezeakis@gmail.com>
2021-10-01 06:20:06 +0300

    Update machine_learning.py

--- final_flask_code/machine_learning.py
+++ final_flask_code/machine_learning.py
@@ -12,22 +12,6 @@ from sklearn.linear_model import LogisticRegression
 logisticRegr = LogisticRegression()
 logisticRegr.fit(x_train, y_train)
 
-def result():
-    return "hi"
-
-for sample_id in range(0, 20):
-    print("id: {}".format(sample_id))
-    mytest = x_test.iloc[sample_id, :]
-    type(x_test)
-    print(mytest)
-    predicted_value = logisticRegr.predict(mytest.values.reshape(1, -1))
-    predicted_probs = logisticRegr.predict_proba(mytest.values.reshape(1,-1))
-    print(predicted_probs)
-    print("predicted_value: {}".format(predicted_value))
-    print(predicted_probs[0][predicted_value])
-    print("actual value: {}".format(y_test.iloc[sample_id]))
-
-
 def result2(mytest):
     predicted_value = logisticRegr.predict(mytest)
     predicted_probs = logisticRegr.predict_proba(mytest)
